8afd137ce5877aa1e01bfab2cf39a0dd200a69f3
yangzheng <yz6780610>
2020-10-31 14:06:01 +0800

    Initial commit

---
+++ src/main/java/com/example/springboottest/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.springboottest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yangzheng
+ * @version 1.0
+ * @description
+ * @date 2020/10/31 031 13:54
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
